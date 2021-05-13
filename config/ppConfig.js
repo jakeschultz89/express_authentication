d40f72666e68a07306a76d06e18b947bcef063ff
jakeschultz89 <ejacobschultz@gmail.com>
2021-05-13 14:55:31 -0400

    ppConfig: Create passport

---
+++ config/ppConfig.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+
+// database
+const db = require('../models');
+
+const STRATEGY = new LocalStrategy({
+    usernameField: 'email',         // looks for an email field as the username
+    passwordField: 'password'       // looks for an password field as the password
+    }, async (email, password, cb) => {
+        try {
+            const user = await db.user.findOne({
+                where: { email }
+            });
+            if (!user || !user.validPassword(password)) { 
+                cb(null, false); // if no user or invalid password, return false
+            } else {
+                cb(null, user);
+            }
+        } catch (err) {
+            console.log('--- Error ---');
+            console.log(err);
+        }
+});
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(async (id, cb) => {
+    try {
+        const user = await db.user.findByPk(id);
+
+        if (user) {
+            cb(null, user);
+        }
+
+    } catch (error) {
+        console.log('--- Yo, there is an error below ---');
+        console.log(error);
+    }
+});
+
+passport.use(STRATEGY);
+
+module.exports = passport;
